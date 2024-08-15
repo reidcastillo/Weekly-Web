df9cc5d6ab08dbf0534a03d1e8cf9cdd2d2dbb19
reidcastillo <reid.castillo@ufl.edu>
2024-08-15 19:59:45 -0400

    Links working

--- app.py
+++ app.py
@@ -623,9 +623,5 @@ def property():
     '''
     return html_content
 
-
-
-
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80, debug=True)
+    app.run(host='0.0.0.0', port=443, debug=True, ssl_context=('cert.pem', 'key.pem'))
